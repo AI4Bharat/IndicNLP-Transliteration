87b6aef95071ec3541c1e721068b5b1a93a1222a
Gokul NC <gokulnc@ymail.com>
2020-01-29 09:21:38 +0000

    Add separate module to set standardized constants

---
+++ utilities/constants.py
@@ -0,0 +1,14 @@
+import os
+from os.path import join
+
+LOSS_FILE = 'train_loss.txt'
+SCORES_FILE = 'scores.txt'
+MODEL_FILE = 'model-e%d.pt'
+
+def get_train_files(ckpt_folder):
+    if not os.path.isdir(ckpt_folder):
+        os.makedirs(ckpt_folder, exist_ok=True)
+    loss_file = join(ckpt_folder, LOSS_FILE)
+    scores_file = join(ckpt_folder, SCORES_FILE)
+    model_file = join(ckpt_folder, MODEL_FILE)
+    return loss_file, scores_file, model_file
