7213bfd70a8fa6a85aaf81c2cddbf2b00c97648a
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-03-25 11:08:44 +0530

    [task] RNN trainer

--- tasks/rnn_runner.py
+++ tasks/rnn_runner.py
@@ -7,6 +7,7 @@ import os
 import sys
 import utilities.running_utils as rutl
 from utilities.lang_data_utils import XlitData, GlyphStrawboss
+from utilities.logging_utils import LOG2CSV
 from algorithms.recurrent_nets import Encoder, Decoder, Seq2Seq
 
 
@@ -17,27 +18,34 @@ INST_NAME = "Training_101"
 ##------------------------------------------------------------------------------
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = "cpu"
-if not os.path.exists("logs/"+INST_NAME): os.makedirs("logs/"+INST_NAME)
-
+LOG_PATH = "logs/"+INST_NAME+"/"
+WGT_PATH = "hypotheses/"+INST_NAME+"/"
+if not os.path.exists(LOG_PATH): os.makedirs(LOG_PATH)
+if not os.path.exists(WGT_PATH): os.makedirs(WGT_PATH)
 
 ##===== Running Configuration =================================================
 
 src_glyph = GlyphStrawboss("en")
 tgt_glyph = GlyphStrawboss("hi")
 
-num_epochs = 10
-batch_size = 2
+num_epochs = 1000
+batch_size = 1
 acc_grad = 1
 learning_rate = 1e-5
 pretrain_wgt_path = None
 
 
 train_dataset = XlitData( src_glyph_obj = src_glyph, tgt_glyph_obj = tgt_glyph,
-                        json_file='data/HiEn_fire13_dev.json', file_map = "LangEn",
+                        json_file='data/checkup-train.json', file_map = "LangEn",
                         padding=True)
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
                                 shuffle=True, num_workers=0)
 
+val_dataset = XlitData( src_glyph_obj = src_glyph, tgt_glyph_obj = tgt_glyph,
+                        json_file='data/checkup-train.json', file_map = "LangEn",
+                        padding=True)
+val_dataloader = DataLoader(train_dataset, batch_size=batch_size,
+                                shuffle=True, num_workers=0)
 
 # for i in range(len(train_dataset)):
 #     print(train_dataset.__getitem__(i))
@@ -72,12 +80,14 @@ model = Seq2Seq(enc, dec).to(device)
 criterion = torch.nn.CrossEntropyLoss()
 
 def loss_estimator(pred, truth):
-    """ Only consider non-zero inputs in the loss; mask needed """
-    #mask = 1 - np.equal(real, 0) # assign 0 to all above 0 and 1 to all 0s
-    #print(mask)
+    """ Only consider non-zero inputs in the loss; mask needed
+    pred: batch
+    """
+    pred = torch.cat(torch.unbind(pred, dim=0))
+    truth = torch.cat(torch.unbind(truth, dim=0))
 
     mask = truth.ge(1).type(torch.FloatTensor)
-    loss_ = criterion(pred, truth) * mask.device()
+    loss_ = criterion(pred, truth) * mask
     return torch.mean(loss_)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
@@ -90,7 +100,7 @@ if __name__ =="__main__":
 
     best_loss = float("inf")
     for epoch in range(num_epochs):
-        #--- Train
+        #-------- Training -------------------
         acc_loss = 0
         running_loss = []
         for ith, (src, tgt, src_sz, tgt_sz) in enumerate(train_dataloader):
@@ -98,48 +108,48 @@ if __name__ =="__main__":
             src = src.to(device)
             tgt = tgt.to(device)
 
-            #--- forward
+            #------ forward ------
             output = model(src, tgt, src_sz, tgt_sz)
-            print("Out shape", output.shape)
-            loss = loss_estimator(output, tgt) / acc_batch
+            loss = loss_estimator(output, tgt) / acc_grad
             acc_loss += loss
 
-            #--- backward
+            #------ backward ------
             loss.backward()
-            if ( (ith+1) % acc_batch == 0):
+            if ( (ith+1) % acc_grad == 0):
                 optimizer.step()
                 optimizer.zero_grad()
                 print('epoch[{}/{}], Mini Batch-{} loss:{:.4f}'
-                    .format(epoch+1, num_epochs, (ith+1)//acc_batch, acc_loss.data))
-                running_loss.append(acc_loss.data)
+                    .format(epoch+1, num_epochs, (ith+1)//acc_grad, acc_loss.data))
+                running_loss.append(acc_loss.item())
                 acc_loss=0
                 #break
-        log_to_csv(running_loss, "logs/"+INST_NAME+"/trainLoss.csv")
 
-        #--- Validate
+        LOG2CSV(running_loss, LOG_PATH+"trainLoss.csv")
+
+        #--------- Validate ---------------------
         val_loss = 0
         val_accuracy = 0
-        for jth, (val_src, val_tgt, val_src_sz) in enumerate(test_dataloader):
-            val_src = val_src.to(device)
-            val_tgt = val_tgt.to(device)
+        for jth, (v_src, v_tgt, v_src_sz, v_tgt_sz) in enumerate(val_dataloader):
+            v_src = v_src.to(device)
+            v_tgt = v_tgt.to(device)
             with torch.no_grad():
-                val_output = model(val_img)
-                val_loss += loss_estimator(val_output, val_tgt)
+                v_output = model(v_src, v_tgt, v_src_sz, v_tgt_sz)
+                val_loss += loss_estimator(v_output, v_tgt)
 
                 val_accuracy += (1 - val_loss)
             #break
-        val_loss = val_loss / len(test_dataloader)
-        val_accuracy = val_accuracy / len(test_dataloader)
+        val_loss = val_loss / len(val_dataloader)
+        val_accuracy = val_accuracy / len(val_dataloader)
 
         print('epoch[{}/{}], [-----TEST------] loss:{:.4f}  Accur:{:.4f}'
               .format(epoch+1, num_epochs, val_loss.data, val_accuracy.data))
-        log_to_csv([val_loss.item(), val_accuracy.item()],
-                    "logs/"+INST_NAME+"/testLoss.csv")
+        LOG2CSV([val_loss.item(), val_accuracy.item()],
+                    LOG_PATH+"valLoss.csv")
 
-        #--- save Checkpoint
+        #-------- save Checkpoint -------------------
         if val_loss < best_loss:
             print("***saving best optimal state [Loss:{}] ***".format(val_loss.data))
             best_loss = val_loss
-            torch.save(model.state_dict(), "weights/"+INST_NAME+"_model.pth")
-            log_to_csv([epoch+1, val_loss.item(), val_accuracy.item()],
-                    "logs/"+INST_NAME+"/bestCheckpoint.csv")
+            torch.save(model.state_dict(), WGT_PATH+INST_NAME+"_model-{}.pth".format(epoch))
+            LOG2CSV([epoch+1, val_loss.item(), val_accuracy.item()],
+                    LOG_PATH+"bestCheckpoint.csv")
