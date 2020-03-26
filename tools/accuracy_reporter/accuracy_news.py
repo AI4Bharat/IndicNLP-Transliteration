ab6a1010b8bd7450756757e9e89b4a6b29433cc3
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-03-26 21:25:48 +0530

    [tools] Accuracy estimations scripts and routines

--- tools/accuracy_reporter/accuracy_news.py
+++ tools/accuracy_reporter/accuracy_news.py
@@ -5,11 +5,11 @@ given the predictions and ground truth as JSONs.
 USAGE:
     Example 1: Predictions file format - JSON
     <<script.py>> --gt-json EnHi_dev.json --pred-json Google/EnHi_dev.json [--save-output-csv scores.csv]
-    
+
     Example 2: Predictions file format - txt
     <<script.py>> --gt-json EnHi_dev.json --in EnHi_dev.en --out Moses_out.hi [--save-output-csv scores.csv]
 
-Taken from: https://github.com/snukky/news-translit-nmt/blob/master/tools/news_evaluation.py
+Source: https://github.com/snukky/news-translit-nmt/blob/master/tools/news_evaluation.py
 """
 
 import os, sys, json, tqdm
@@ -26,7 +26,7 @@ def LCS_length(s1, s2):
     C = [[0] * (n+1) for i in range(m+1)]
     for i in range(1, m+1):
         for j in range(1, n+1):
-            if s1[i-1] == s2[j-1]: 
+            if s1[i-1] == s2[j-1]:
                 C[i][j] = C[i-1][j-1] + 1
             else:
                 C[i][j] = max(C[i][j-1], C[i-1][j])
@@ -45,7 +45,7 @@ def f_score(candidate, references):
         if (len(ref) - 2*lcs) < (len(best_ref) - 2*best_ref_lcs):
             best_ref = ref
             best_ref_lcs = lcs
-    
+
     try:
         precision = float(best_ref_lcs)/float(len(candidate))
         recall = float(best_ref_lcs)/float(len(best_ref))
@@ -53,24 +53,24 @@ def f_score(candidate, references):
         return 0.0, best_ref
     #    import ipdb
     #    ipdb.set_trace()
-    
+
     if best_ref_lcs:
         return 2*precision*recall/(precision+recall), best_ref
     else:
         return 0.0, best_ref
-    
+
 def mean_average_precision(candidates, references, n):
     '''
     Calculates mean average precision up to n candidates.
     '''
-    
+
     total = 0.0
     num_correct = 0
     for k in range(n):
         if k < len(candidates) and (candidates[k] in references):
             num_correct += 1
         total += float(num_correct)/float(k+1)
-        
+
     return total/float(n)
 
 def inverse_rank(candidates, reference):
@@ -99,24 +99,24 @@ def evaluate(input_data, test_data, verbose=True):
     #map_n = {}
     map_ref = {}
     #map_sys = {}
-    
+
     empty_xlits = []
-    
+
     for src_word in test_data.keys():
         if src_word in input_data and len(input_data[src_word]) > 0:
             candidates = input_data[src_word]
             references = test_data[src_word]
-            
+
             acc[src_word] = max([int(candidates[0] == ref) for ref in references]) # either 1 or 0
-            
+
             f[src_word], f_best_match[src_word] = f_score(candidates[0], references)
-            
+
             mrr[src_word] = max([inverse_rank(candidates, ref) for ref in references])
-            
+
             #map_n[src_word] = mean_average_precision(candidates, references, n)
             map_ref[src_word] = mean_average_precision(candidates, references, len(references))
             #map_sys[src_word] = mean_average_precision(candidates, references, len(candidates))
-            
+
         else:
             empty_xlits.append(src_word)
             mrr[src_word] = 0.0
@@ -126,11 +126,11 @@ def evaluate(input_data, test_data, verbose=True):
             #map_n[src_word] = 0.0
             map_ref[src_word] = 0.0
             #map_sys[src_word] = 0.0
-        
+
     if len(empty_xlits) > 0 and verbose:
         print('Warning: No transliterations found for following %d words out of %d:' % (len(empty_xlits), len(test_data.keys())))
         print(empty_xlits)
-            
+
     return acc, f, f_best_match, mrr, map_ref
 
 def write_details(output_fname, input_data, test_data, acc, f, f_best_match, mrr, map_ref):
@@ -138,18 +138,18 @@ def write_details(output_fname, input_data, test_data, acc, f, f_best_match, mrr
     Writes detailed results to CSV file
     '''
     f_out = open(output_fname, 'w', encoding='utf-8')
-    
+
     f_out.write('%s\n' % (','.join(['"Source word"', '"First candidate"', '"Top-1"', '"ACC"', '"F-score"', '"Best matching reference"',
     '"MRR"', '"MAP_ref"', '"References"'])))
-    
+
     for src_word in test_data.keys():
         if src_word in input_data and len(input_data[src_word]) > 0:
             first_candidate = input_data[src_word][0]
         else:
             first_candidate = ''
-            
+
         f_out.write('%s,%s,%f,%f,%s,%f,%f,%s\n' % (src_word, first_candidate, acc[src_word], f[src_word], f_best_match[src_word], mrr[src_word], map_ref[src_word], '"' + ' | '.join(test_data[src_word]) + '"'))
-    
+
     f_out.close()
 
 def read_data(gt_json, args):
@@ -160,7 +160,7 @@ def read_data(gt_json, args):
     # Read test data ground truth
     with open(gt_json, encoding='utf8') as f:
             gt_data = json.load(f)
-    
+
     # Read Predictions based on file_type
     if file_type.lower() == 'json':
         pred_json = args.pred_json
@@ -170,9 +170,9 @@ def read_data(gt_json, args):
         # Read predictions
         with open(pred_json, encoding='utf8') as f:
             pred_data = json.load(f)
-            
+
         return gt_data, pred_data
-    
+
     elif file_type.lower() == 'txt':
         # Read predictions from 2 parallel txt files
         infile = args.inp
@@ -181,17 +181,17 @@ def read_data(gt_json, args):
             sys.exit('ERROR:', infile, 'Not Found')
         if not os.path.isfile(outfile):
             sys.exit('ERROR:', outfile, 'Not Found')
-        
+
         # Read all the input and output lines into memory
         with open(infile) as f:
             input_lines = f.readlines()
         with open(outfile) as f:
             output_lines = f.readlines()
-        
+
         if len(input_lines) != len(output_lines):
             sys.exit('ERROR: The number of lines in input (%d) and output (%d) do not match!'
                     % (len(input_lines), len(output_lines)))
-        
+
         pred_data = {}
         # Assumes one word per line
         for inp, out in zip(input_lines, output_lines):
@@ -200,14 +200,14 @@ def read_data(gt_json, args):
             if inp not in pred_data:
                 pred_data[inp] = []
             pred_data[inp].append(out)
-            
+
         if args.save_json:
             # Save as JSON for future reference
             with open(args.save_json, 'w', encoding='utf8') as f:
                 json.dump(pred_data, f, ensure_ascii=False, indent=4, sort_keys=True)
-        
+
         return gt_data, pred_data
-    
+
     else:
         sys.exit('ERROR: Unrecognized predictions file format:', file_type)
 
---
+++ tools/accuracy_reporter/orchestrator.py
@@ -0,0 +1,85 @@
+""" Simple automation
+To be run from Repo Root directory
+"""
+import os
+import json
+
+def toggle_json(read_path, save_prefix=""):
+    with open(read_path, 'r', encoding = "utf-8") as f:
+        data = json.load(f)
+
+    tog_dict = dict()
+    for d in data.keys():
+        for v in data[d]:
+            tog_dict[v] = set()
+
+    for d in data.keys():
+        for v in data[d]:
+            tog_dict[v].add(d)
+
+    for t in tog_dict.keys():
+        tog_dict[t] = list(tog_dict[t])
+
+    save_file = save_prefix+"Toggled-"+ os.path.basename(path)
+    with open(save_file,"w", encoding = "utf-8") as f:
+        json.dump(tog_dict, f, ensure_ascii=False, indent=4, sort_keys=True,)
+
+    return save_file
+
+def get_from_json(path, ret_data = "key"):
+    with open(path, 'r', encoding = "utf-8") as f:
+        data = json.load(f)
+
+    if ret_data == "key":
+        out = list(data.keys())
+    elif ret_data == "value":
+        out = list(set(data.value()))
+    elif ret_data == "both":
+        out = []
+        for k in data:
+            for v in data[k]:
+                out.append([k,v])
+
+    return sorted(out)
+
+def save_to_json(path, data_dict):
+    with open(path ,"w", encoding = "utf-8") as f:
+        json.dump(data_dict, f, ensure_ascii=False, indent=4, sort_keys=True,)
+
+##------------------------------------------------------------------------------
+
+def inference_looper(in_words):
+    from tasks.infer_engine import hi_inferencer
+    out_dict = {}
+    for i in in_words:
+        out_dict[i] = hi_inferencer(word)
+    return out_dict
+
+ROOT_PATH= ""
+SAVE_DIR = "tools/accuracy_reporter/temp-exp1"
+if not os.path.exists(SAVE_DIR): os.makedirs(SAVE_DIR)
+
+files = [
+    ROOT_PATH+"tools/accuracy_reporter/temp-data/temp-EnHi_fire13_dev.json",
+    ROOT_PATH+"tools/accuracy_reporter/temp-data/temp-EnHi_news18_dev.json",
+    ROOT_PATH+"tools/accuracy_reporter/temp-data/temp-EnHi_varnam_test.json",
+    ROOT_PATH+"tools/accuracy_reporter/temp-data/temp-EnHi_varnam_special_test.json",
+]
+
+if __name__ == "__main__":
+
+    for fi in files:
+        # words = get_from_json(fi, "key")
+        # out_dict = inference_looper(words)
+        # sv_path = os.path.join(SAVE_DIR, "pred_"+os.path.basename(fi) )
+        # save_to_json(sv_path, out_dict)
+
+        gt_json = fi
+        pred_json = sv_path
+        save_prefix = os.path.join(SAVE_DIR, os.path.basename(fi).replace(".json", ""))
+        run_accuracy_news = "python tools/accuracy_reporter/accuracy_news.py --gt-json {} --pred-json {} --save-output-csv {}_scores.csv".format(
+                        gt_json, pred_json, save_prefix)
+
+        os.system(run_accuracy_news)
+
+
