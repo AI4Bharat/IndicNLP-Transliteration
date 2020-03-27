ba990eb5b0e8c00543ed6ed79d3767683957c0f6
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-03-27 16:57:13 +0530

    [tool] Fix timeout program exit, use try-except block

--- tools/translit_engines.py
+++ tools/translit_engines.py
@@ -12,36 +12,56 @@ ENGINES:
 
 import os, sys, json, requests
 from tqdm import tqdm
+import time
 
 TOP_K = 6
 LANG2CODE = {
-	'tamil': 'ta',
-	'hindi': 'hi',
-	'telugu': 'te',
-	'marathi': 'mr',
-	'punjabi': 'pa',
-	'bengali': 'bn',
-	'gujarati': 'gu',
-	'kannada': 'kn',
-	'malayalam': 'ml',
-	'nepali': 'ne'
+    'tamil': 'ta',
+    'hindi': 'hi',
+    'telugu': 'te',
+    'marathi': 'mr',
+    'punjabi': 'pa',
+    'bengali': 'bn',
+    'gujarati': 'gu',
+    'kannada': 'kn',
+    'malayalam': 'ml',
+    'nepali': 'ne'
 }
 
 G_API = 'https://inputtools.google.com/request?text=%s&itc=%s-t-i0-und&num=%d'
 def gtransliterate(word, lang_code, num_suggestions=10):
-	response = requests.request('GET', G_API % (word, lang_code, num_suggestions), allow_redirects=False, timeout=5)
-	r = json.loads(response.text)
-	if 'SUCCESS' not in r[0] or response.status_code != 200:
-		print('Request failed with status code: %d\nERROR: %s' % (response.status_code, response.text), file=sys.stderr)
-		return []
-	return r[1][0][1]
+    CF  = False
+    while not CF:
+        try:
+            response = requests.request('GET', G_API % (word, lang_code, num_suggestions), allow_redirects=False, timeout=5)
+            r = json.loads(response.text)
+
+            if 'SUCCESS' not in r[0] or response.status_code != 200:
+                print('Request failed with status code: %d\nERROR: %s' % (response.status_code, response.text), file=sys.stderr)
+                time.sleep(5)
+            else:
+                CF = True
+        except Exception as err:
+            print("Hit Exception:", err)
+            time.sleep(5)
+
+    return r[1][0][1]
 
 QP_API = 'http://xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=%s&inString=%s'
 def qp_transliterate(word, lang, num_suggestions=10):
-    response = requests.request('GET', QP_API % (lang, word), allow_redirects=False, timeout=5)
-    if response.status_code != 200:
-        print('Request failed with status code: %d\nERROR: %s' % (response.status_code, response.text), file=sys.stderr)
-        return []
+    CF  = False
+    while not CF:
+        try:
+            response = requests.request('GET', QP_API % (lang, word), allow_redirects=False, timeout=5)
+            if response.status_code != 200:
+                print('Request failed with status code: %d\nERROR: %s' % (response.status_code, response.text), file=sys.stderr)
+                time.sleep(5)
+            else:
+                CF = True
+        except Exception as err:
+            print("Hit Exception:", err)
+            time.sleep(5)
+
     r = json.loads(response.text)
     suggestions = r['twords'][0]['options']
     if r['itrans'] not in suggestions:
@@ -56,10 +76,19 @@ def run_qp_bulk(input_list, lang):
 
 VARNAM_API = 'https://api.varnamproject.com/tl/%s/%s'
 def varnam_transliterate(word, lang_code, num_suggestions=10):
-    response = requests.request('GET', VARNAM_API % (lang_code, word), allow_redirects=False, timeout=5)
-    if response.status_code != 200:
-        print('Request failed with status code: %d\nERROR: %s' % (response.status_code, response.text), file=sys.stderr)
-        return []
+    CF  = False
+    while not CF:
+        try:
+            response = requests.request('GET', VARNAM_API % (lang_code, word), allow_redirects=False, timeout=5)
+            if response.status_code != 200:
+                print('Request failed with status code: %d\nERROR: %s' % (response.status_code, response.text), file=sys.stderr)
+                time.sleep(5)
+            else:
+                CF = True
+        except Exception as err:
+            print("Hit Exception:", err)
+            time.sleep(5)
+
     r = json.loads(response.text)
     return r['result'][:num_suggestions]
 
@@ -77,12 +106,12 @@ def predict_transliterations(input_json, input_txt, xlit_engine, lang):
         with open(input_json, encoding='utf8') as f:
             data = json.load(f)
         input_data.update(set(data.keys()))
-    
+
     # Read input data from txt file with one word per line
     if input_txt and os.path.isfile(input_txt):
         with open(input_txt, encoding='utf8') as f:
             for line in f: input_data.add(line.strip())
-    
+
     if 'google' in xlit_engine:
         return run_bulk(input_data, LANG2CODE[lang], gtransliterate)
     elif 'quill' in xlit_engine:
@@ -111,7 +140,7 @@ def __main(args):
 
     if args.lang not in LANG2CODE:
         sys.exit(args.lang, 'language is not supported.')
-    
+
     if args.infer:
         predict_word(args.infer, args.lang, args.xlit_src.lower())
         return
@@ -121,7 +150,7 @@ def __main(args):
 
     predictions = predict_transliterations(args.json, args.txt, args.xlit_src.lower(), args.lang)
     save_predictions(predictions, args.output)
-    
+
 
 if __name__ == '__main__':
     import argparse
@@ -133,4 +162,3 @@ if __name__ == '__main__':
     parser.add_argument('--lang', type=str, required=True)
     parser.add_argument('--output', type=str, required=True)
     __main(parser.parse_args())
-    
