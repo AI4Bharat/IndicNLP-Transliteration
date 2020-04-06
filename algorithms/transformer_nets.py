e95338e84e974462b0ec8f0aab783143613981e4
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-04-06 16:42:01 +0530

    [net] Transformer Basic

--- algorithms/transformer_nets.py
+++ algorithms/transformer_nets.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 
 
 class XFMR_Seq2Seq(nn.Module):
+    '''
+    TODO: Not yet complete Decoding part has to be fixed
+    '''
     def __init__(self, input_vcb_sz, output_vcb_sz,
                     emb_dim, enc_layers, dec_layers,
                     attention_head = 8, feedfwd_dim = 1024,
@@ -60,3 +63,54 @@ class XFMR_Seq2Seq(nn.Module):
         return out
 
 
+class XFMR_Neophyte(nn.Module):
+    def __init__(self, input_vcb_sz, output_vcb_sz,
+                    emb_dim, n_layers,
+                    attention_head = 8, feedfwd_dim = 1024,
+                    dropout = 0,
+                    device = "cpu"):
+        super(XFMR_Neophyte, self).__init__()
+
+        self.input_dim = input_vcb_sz
+        self.output_dim = output_vcb_sz
+        self.vector_dim = emb_dim  # same size will be used for all layers in transformer
+        self.atten_head = attention_head
+        self.n_layers = n_layers
+        self.feedfwd_dim = feedfwd_dim
+        self.dropout = dropout
+
+
+        self.in2embed = nn.Embedding(self.input_dim, self.vector_dim)
+
+        _enc_layer = nn.TransformerEncoderLayer(d_model=self.vector_dim,
+                                                nhead=self.atten_head)
+        self.xfmr_enc = nn.TransformerEncoder(_enc_layer, num_layers= n_layers)
+
+        self.out_fc = nn.Sequential( nn.LeakyReLU(),
+            nn.Linear(self.vector_dim, self.output_dim)
+            )
+
+    def forward(self, src):
+        '''
+        src: (batch, in_seq_len-padded)
+        tgt: (batch, out_seq_len-padded)
+        '''
+
+        # src_emb: (batch, in_seq_len, vector_dim)
+        src_emb = self.in2embed(src)
+
+        # src_emb: (seq_len,batch,vector_dim) -> for transformer
+        src_emb = src_emb.permute(1,0,2)
+
+        # out: (out_seq_len,batch,vector_dim)
+        out = self.xfmr_enc(src_emb)
+
+        # out: (batch,out_seq_len,vector_dim)
+        out = out.permute(1,0,2).contiguous()
+
+        # out: (batch,out_seq_len,out_vcb_dim)
+        out = self.out_fc(out)
+        # out: (batch,out_vcb_dim, out_seq_len)
+        out = out.permute(0,2,1).contiguous()
+
+        return out
