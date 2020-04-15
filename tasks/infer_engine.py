c1470c51babe2fa6a65ee1e7aee978bdf1c91953
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-04-15 23:06:58 +0530

    [net] Beam search based decoding for RNN

--- tasks/infer_engine.py
+++ tasks/infer_engine.py
@@ -13,11 +13,17 @@ weights = torch.load( weight_path, map_location=torch.device('cpu'))
 model.load_state_dict(weights)
 model.eval()
 
-def inferencer(word):
-    in_vec = torch.from_numpy(en_glyph.word2xlitvec(word))
-    out = model.inference(in_vec)
-    result = hi_glyph.xlitvec2word(out.numpy())
-    return [result]
+def inferencer(word, topk = 3):
+    if topk == 1:
+        in_vec = torch.from_numpy(en_glyph.word2xlitvec(word))
+        out = model.inference(in_vec)
+        result =[ hi_glyph.xlitvec2word(out.numpy()) ]
+        return result
+    else:
+        in_vec = torch.from_numpy(en_glyph.word2xlitvec(word))
+        out_list = model.beam_inference(in_vec, beam_width = topk)
+        result = [ hi_glyph.xlitvec2word(out.numpy()) for out in out_list]
+        return result
 
 
 if __name__ == "__main__":
