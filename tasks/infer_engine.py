9224512dd89f508984028e9842cacf3713cd66d8
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-06-08 15:53:30 +0530

    [fix] Numpy copy issues from GPU

--- tasks/infer_engine.py
+++ tasks/infer_engine.py
@@ -4,16 +4,16 @@ import utilities.running_utils as rutl
 
 hi_glyph = GlyphStrawboss("hi")
 en_glyph = GlyphStrawboss("en")
+device = "cpu"
 
 ##=============== Models =======================================================
 import torch
-DEVICE = "cpu"
 
 from hypotheses.training_mai_103.recurrent_nets_mai_103 import model
 weight_path = "hypotheses/training_mai_103/weights/Training_mai_103_model.pth"
 # voc_sanitize = VocabSanitizer("data/X_word_list.json")
 
-weights = torch.load( weight_path, map_location=torch.device(DEVICE))
+weights = torch.load( weight_path, map_location=torch.device(device))
 model.load_state_dict(weights)
 model.eval()
 
@@ -21,7 +21,7 @@ model.eval()
 from hypotheses.training_mai_103.recurrent_nets_mai_103 import corr_model
 corr_weight_path = "hypotheses/training_mai_103/weights/Training_mai_103_corrnet.pth"
 
-corr_weights = torch.load( corr_weight_path, map_location=torch.device(DEVICE))
+corr_weights = torch.load( corr_weight_path, map_location=torch.device(device))
 corr_model.load_state_dict(corr_weights)
 corr_model.eval()
 
@@ -30,17 +30,17 @@ corr_model.eval()
 
 def inferencer(word, topk = 3):
     if topk == 1:
-        in_vec = torch.from_numpy(en_glyph.word2xlitvec(word))
+        in_vec = torch.from_numpy(en_glyph.word2xlitvec(word)).to(device)
         out = model.inference(in_vec)
         out = corr_model.inference(out)
-        result =[ hi_glyph.xlitvec2word(out.numpy()) ]
+        result =[ hi_glyph.xlitvec2word(out.cpu().numpy()) ]
         return result
     else:
-        in_vec = torch.from_numpy(en_glyph.word2xlitvec(word))
+        in_vec = torch.from_numpy(en_glyph.word2xlitvec(word)).to(device)
         ## change to active or passive beam
         out_list = model.active_beam_inference(in_vec, beam_width = topk)
         out_list = [ corr_model.inference(out) for out in out_list]
-        result = [ hi_glyph.xlitvec2word(out.numpy()) for out in out_list]
+        result = [ hi_glyph.xlitvec2word(out.cpu().numpy()) for out in out_list]
         # result = voc_sanitize.reposition(result)
         return result
 
