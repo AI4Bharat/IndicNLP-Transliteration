[Fix] Accuracy metric computation during training

algorithms/recurrent_nets.py
@@ -260,7 +260,7 @@ class Seq2Seq(nn.Module):
 
         # dec_input: (batch_size, 1)
         dec_input = tgt[:,0].unsqueeze(1) # initialize to start token
-
+        pred_vecs[:,1,0] = 1 # Initialize to start tokens all batches
         for t in range(1, tgt.size(1)):
             # dec_hidden: dec_layers, batch_size , dec_hidden_dim
             # dec_output: batch_size, output_dim
@@ -312,7 +312,7 @@ class Seq2Seq(nn.Module):
 
         # dec_input: (batch_size, 1)
         dec_input = start_tok.view(1,1) # initialize to start token
-
+        pred_arr[0] = start_tok.view(1,1) # initialize to start token
         for t in range(max_tgt_sz):
             # dec_hidden: dec_layers, batch_size , dec_hidden_dim
             # dec_output: batch_size, output_dim

utilities/running_utils.py
@@ -31,7 +31,7 @@ def count_train_param(model):
     return train_params_count
 
 
-def accuracy_score(pred_tnsr, tgt_tnsr):
+def accuracy_score(pred_tnsr, tgt_tnsr, glyph_obj):
     '''Simple accuracy calculation for TRAINING phase
     pred_tnsr: torch tensor :shp: (batch, seq_len)
     tgt_tnsr: torch tensor :shp: (batch, voc_size, seq_len)
@@ -40,7 +40,9 @@ def accuracy_score(pred_tnsr, tgt_tnsr):
     batch_sz = pred_seq.shape[0]
     crt_cnt = 0
     for i in range(batch_sz):
-        if torch.equal(pred_seq[i,:], tgt_tnsr[i, :]):
+        pred = glyph_obj.xlitvec2word(pred_seq[i,:].cpu().numpy())
+        tgt = glyph_obj.xlitvec2word(tgt_tnsr[i,:].cpu().numpy())
+        if pred == tgt:
             crt_cnt += 1
     return torch.tensor(crt_cnt/batch_sz)
 