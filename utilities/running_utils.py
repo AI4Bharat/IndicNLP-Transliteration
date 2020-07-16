b5e394db8ecc0a67b870d736c0136483eb8a6cfd
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-07-16 16:43:16 +0530

    [utils] Update to nagari chars and loading weight functions

--- utilities/running_utils.py
+++ utilities/running_utils.py
@@ -11,14 +11,15 @@ class RunMode(enum.Enum):
     infer = "inference"
     test  = "testing"
 
-def load_pretrained(model, weight_path):
+def load_pretrained(model, weight_path, flexible = False):
     if not weight_path:
         return model
 
     pretrain_dict = torch.load(weight_path)
     model_dict = model.state_dict()
-    pretrain_dict = {k: v for k, v in pretrain_dict.items() if k in model_dict}
-    print("Pretrained layers Loaded:", pretrain_dict.keys())
+    if flexible:
+        pretrain_dict = {k: v for k, v in pretrain_dict.items() if k in model_dict}
+    print("Pretrained layers:", pretrain_dict.keys())
     model_dict.update(pretrain_dict)
     model.load_state_dict(model_dict)
 
