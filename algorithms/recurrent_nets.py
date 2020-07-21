09b16eb8f9a22a8552ca03be2816fc90dc07393a
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-07-21 17:40:51 +0530

    [LM] Char based LM sweep change

--- algorithms/recurrent_nets.py
+++ algorithms/recurrent_nets.py
@@ -34,7 +34,7 @@ class Encoder(nn.Module):
 
     def forward(self, x, x_sz, hidden = None):
         """
-        src_sz: (batch_size, 1) -  Unpadded sequence lengths used for pack_pad
+        x_sz: (batch_size, 1) -  Unpadded sequence lengths used for pack_pad
         """
         batch_sz = x.shape[0]
         # x: batch_size, max_length, enc_embed_dim
@@ -57,6 +57,33 @@ class Encoder(nn.Module):
         output = output.permute(1,0,2)
 
         return output, hidden
+
+    def get_word_embedding(self, x):
+        """
+        """
+        x_sz = torch.tensor([len(src)])
+        # x: 1, max_length, enc_embed_dim
+        x = self.embedding(x)
+
+        ## pack the padded data
+        # x: max_length, 1, enc_embed_dim -> for pack_pad
+        x = x.permute(1,0,2)
+        x = nn.utils.rnn.pack_padded_sequence(x, x_sz, enforce_sorted=False) # unpad
+
+        # output: packed_size, 1, enc_embed_dim
+        # hidden: n_layer**num_directions, 1, hidden_dim | if LSTM (h_n, c_n)
+        output, hidden = self.enc_rnn(x) # gru returns hidden state of all timesteps as well as hidden state at last timestep
+
+        ## pad the sequence to the max length in the batch
+        # output: max_length, 1, hidden_dim)
+        output, _ = nn.utils.rnn.pad_packed_sequence(output)
+
+        out_embed = torch.sum( output.squeeze(1), axis = 0)
+        if self.enc_embed_dim == self.enc_hidden_dim:
+            out_embed = out_embed + torch.sum(hidden.squeeze(1), axis = 0)
+
+        return out_embed
+
 
 
 
