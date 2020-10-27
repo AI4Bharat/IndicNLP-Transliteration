f0cffe5dd52946f2793fa260bce48197e6aea2a1
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2020-10-27 15:33:10 +0530

    [app] inferencing restructuring to class

--- apps/xlit_src.py
+++ apps/xlit_src.py
@@ -213,6 +213,9 @@ class Decoder(nn.Module):
 
 
 class Seq2Seq(nn.Module):
+    """
+    Class dependency: Encoder, Decoder
+    """
     def __init__(self, encoder, decoder, pass_enc2dec_hid=False, dropout = 0, device = "cpu"):
         super(Seq2Seq, self).__init__()
 
@@ -345,8 +348,9 @@ class GlyphStrawboss():
             # Smallcase alone
             self.glyphs = [chr(alpha) for alpha in range(97, 122+1)]
         else:
-            glyph_data = json.load(open(glyphs))
-            self.glyphs = glyph_data["glyphs"]
+            self.dossier = json.load(open(glyphs))
+            self.glyphs = self.dossier["glyphs"]
+            self.numsym_map = self.dossier["numsym_map"]
 
         self.char2idx = {}
         self.idx2char = {}
@@ -433,139 +437,147 @@ class VocabSanitizer():
 
 ##=============== INSTANTIATION ================================================
 
+class XlitCrankshaft():
+    """
+    Class dependency: Seq2Seq, GlyphStrawboss, VocabSanitizer
+    Global Variables: F_DIR
+    """
+    def __init__(self):
 
-device = "cpu"
-en_glyph = GlyphStrawboss("en")
-gom_glyph = GlyphStrawboss("mai")
-voc_sanitize = VocabSanitizer(F_DIR+"/mai_all_words_sorted.json")
+        self.device = "cpu"
+        self.in_glyph_obj = GlyphStrawboss("en")
+        self.tgt_glyph_obj = GlyphStrawboss(glyphs = F_DIR+"/models/tamil/ta_scripts.json")
+        self.voc_sanity = VocabSanitizer(F_DIR+"/models/tamil/ta_words_a4b.json")
 
-input_dim = en_glyph.size()
-output_dim = gom_glyph.size()
-enc_emb_dim = 300
-dec_emb_dim = 300
-enc_hidden_dim = 512
-dec_hidden_dim = 512
-rnn_type = "lstm"
-enc2dec_hid = True
-attention = True
-enc_layers = 1
-dec_layers = 2
-m_dropout = 0
-enc_bidirect = True
-enc_outstate_dim = enc_hidden_dim * (2 if enc_bidirect else 1)
-
-enc = Encoder(  input_dim= input_dim, embed_dim = enc_emb_dim,
-                hidden_dim= enc_hidden_dim,
-                rnn_type = rnn_type, layers= enc_layers,
-                dropout= m_dropout, device = device,
-                bidirectional= enc_bidirect)
-dec = Decoder(  output_dim= output_dim, embed_dim = dec_emb_dim,
-                hidden_dim= dec_hidden_dim,
-                rnn_type = rnn_type, layers= dec_layers,
-                dropout= m_dropout,
-                use_attention = attention,
-                enc_outstate_dim= enc_outstate_dim,
-                device = device,)
-
-model = Seq2Seq(enc, dec, pass_enc2dec_hid=enc2dec_hid, device=device)
-model = model.to(device)
-weights = torch.load( F_DIR+"/mai_prediction120.pth", map_location=torch.device(device))
-model.load_state_dict(weights)
-model.eval()
-
-def character_model(word, topk = 5):
-    in_vec = torch.from_numpy(en_glyph.word2xlitvec(word)).to(device)
-    ## change to active or passive beam
-    p_out_list = model.active_beam_inference(in_vec, beam_width = topk)
-    p_result = [ gom_glyph.xlitvec2word(out.cpu().numpy()) for out in p_out_list]
-
-    result = voc_sanitize.reposition(p_result)
-
-    #List type
-    return result
-
-def numsym_model(seg):
-    ''' native_numsym[x] returns a list object
-    '''
-    if len(seg) == 1:
-        return native_numsym[n] + [seg]
-
-    a = [native_numsym[n][0] for n in seg]
-    return ["".join(a)] + [seg]
+        self._numsym_set = set("1234567890.")
+        self._inchar_set = set("abcdefghijklmnopqrstuvwxyz")
+        self._natscr_set = set().union(self.tgt_glyph_obj.glyphs,
+                            sum(self.tgt_glyph_obj.numsym_map.values(),[]) )
 
 
-def word_segementer(sequence):
-    num_set = set("1234567890.")
-    char_set = set("abcdefghijklmnopqrstuvwxyz")
-    nat_set = set().union(native_scripts, native_numsym.values())
+        ## Model Config Static                TODO: add defining in json support
+        input_dim = self.in_glyph_obj.size()
+        output_dim = self.tgt_glyph_obj.size()
+        enc_emb_dim = 300
+        dec_emb_dim = 300
+        enc_hidden_dim = 512
+        dec_hidden_dim = 512
+        rnn_type = "lstm"
+        enc2dec_hid = True
+        attention = True
+        enc_layers = 1
+        dec_layers = 2
+        m_dropout = 0
+        enc_bidirect = True
+        enc_outstate_dim = enc_hidden_dim * (2 if enc_bidirect else 1)
+
+        enc = Encoder(  input_dim= input_dim, embed_dim = enc_emb_dim,
+                        hidden_dim= enc_hidden_dim,
+                        rnn_type = rnn_type, layers= enc_layers,
+                        dropout= m_dropout, device = self.device,
+                        bidirectional= enc_bidirect)
+        dec = Decoder(  output_dim= output_dim, embed_dim = dec_emb_dim,
+                        hidden_dim= dec_hidden_dim,
+                        rnn_type = rnn_type, layers= dec_layers,
+                        dropout= m_dropout,
+                        use_attention = attention,
+                        enc_outstate_dim= enc_outstate_dim,
+                        device = self.device,)
+        self.model = Seq2Seq(enc, dec, pass_enc2dec_hid=enc2dec_hid, device=self.device)
+        self.model = self.model.to(self.device)
+        weights = torch.load( F_DIR+"/models/tamil/ta_101_model.pth", map_location=torch.device(self.device))
+
+        self.model.load_state_dict(weights)
+        self.model.eval()
+
+    def character_model(self, word, topk = 10):
+        in_vec = torch.from_numpy(self.in_glyph_obj.word2xlitvec(word)).to(self.device)
+        ## change to active or passive beam
+        p_out_list = self.model.active_beam_inference(in_vec, beam_width = topk)
+        p_result = [ self.tgt_glyph_obj.xlitvec2word(out.cpu().numpy()) for out in p_out_list]
+
+        result = self.voc_sanity.reposition(p_result)
+
+        #List type
+        return result
+
+    def numsym_model(self, seg):
+        ''' tgt_glyph_obj.numsym_map[x] returns a list object
+        '''
+        if len(seg) == 1:
+            return self.tgt_glyph_obj.numsym_map[n] + [seg]
+
+        a = [self.tgt_glyph_obj.numsym_map[n][0] for n in seg]
+        return [seg] + ["".join(a)]
 
 
-    sequence = sequence.lower()
-    accepted = set().union(num_set, char_set, nat_set)
-    sequence = ''.join([i for i in sequence if i in accepted])
+    def _word_segementer(self, sequence):
 
-    segment = []
-    idx = 0
-    seq_ = list(sequence)
-    while len(seq_):
-        # for Number-Symbol
-        temp = ""
-        while len(seq_) and seq_[0] in num_set:
-            temp += seq_[0]
-            seq_.pop(0)
-        if temp != "": segment.append(temp)
+        sequence = sequence.lower()
+        accepted = set().union(self._numsym_set, self._inchar_set, self._natscr_set)
+        sequence = ''.join([i for i in sequence if i in accepted])
 
-        # for Target Chars
-        temp = ""
-        while len(seq_) and seq_[0] in nat_set:
-            temp += seq_[0]
-            seq_.pop(0)
-        if temp != "": segment.append(temp)
+        segment = []
+        idx = 0
+        seq_ = list(sequence)
+        while len(seq_):
+            # for Number-Symbol
+            temp = ""
+            while len(seq_) and seq_[0] in self._numsym_set:
+                temp += seq_[0]
+                seq_.pop(0)
+            if temp != "": segment.append(temp)
 
-        # for Input-Roman Chars
-        temp = ""
-        while len(seq_) and seq_[0] in char_set:
-            temp += seq_[0]
-            seq_.pop(0)
-        if temp != "": segment.append(temp)
+            # for Target Chars
+            temp = ""
+            while len(seq_) and seq_[0] in self._natscr_set:
+                temp += seq_[0]
+                seq_.pop(0)
+            if temp != "": segment.append(temp)
 
-    return segment
+            # for Input-Roman Chars
+            temp = ""
+            while len(seq_) and seq_[0] in self._inchar_set:
+                temp += seq_[0]
+                seq_.pop(0)
+            if temp != "": segment.append(temp)
 
+        return segment
 
-def inference_engine(sequence):
+    def inferencer(self, sequence):
 
-    num_set = set("1234567890.")
-    char_set = set("abcdefghijklmnopqrstuvwxyz")
-    nat_set = set().union(native_scripts, native_numsym.values())
+        seg = self._word_segementer(sequence[:120])
+        lit_seg = []
 
-    seg = word_segementer(sequence[:120])
-    lit_seg = []
+        # TODO: Fix logic to accomodate up to 5 segments rather than 1st one
+        p = 0; model_flag = True
+        seg_len = len(seg[:3])
+        while p < seg_len:
+            if seg[p][0] in self._natscr_set:
+                lit_seg.append([seg[p]])
+                p+=1; continue
 
-    # TODO: Fix logic to accomodate up to 5 segments rather than 1st one
-    p = 0; model_flag = True
-    seg_len = len(seg[:3])
-    while p < seg_len:
-        if seg[p][0] in nat_set:
-            lit_seg.append([seg[p]])
-            p+=1; continue
+            if model_flag:
+                if seg[p][0] in self._inchar_set:
+                    lit_seg.append(self.character_model(seg[p]))
+                    p+=1; model_flag = False
+                else: # num & punc
+                    lit_seg.append(self.numsym_model(seg[p]))
+                    p+=1; model_flag = False
+            else:
+                p+=1
 
-        if model_flag:
-            if seg[p][0] in char_set:
-                lit_seg.append(character_model(seg[p]))
-                p+=1; model_flag = False
-            else: # num & punc
-                lit_seg.append(numsym_model(seg[p]))
-                p+=1; model_flag = False
-        else:
-            p+=1
+        final_result = [""]
+        for seg in lit_seg:
+            new_result = []
+            for s in seg:
+                for f in final_result:
+                    new_result.append(f+s)
+            final_result = new_result
 
+        return final_result
 
-    final_result = [""]
-    for seg in lit_seg:
-        new_result = []
-        for s in seg:
-            for f in final_result:
-                new_result.append(f+s)
-        final_result = new_result
+if __name__ == "__main__":
 
-    return final_result
+    handle = XlitCrankshaft()
+    print(handle.inferencer("123"))
